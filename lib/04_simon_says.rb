99be2632259f7924a29c57a8b057e09ece2f5477
KarRobin <robindebx@gmail.com>
2020-04-14 09:18:35 +0200

    aj

--- lib/04_simon_says.rb
+++ lib/04_simon_says.rb
@@ -1,17 +1,28 @@
-def echo(str)
-    return "#{str}"
-end
 
-def shout(str)
-    return str.upcase
-end
 
-def repeat(str,n)
-    a = "#{str}" * n
-    return a.inject " "
+    def echo(str)
+        return "#{str}"
+    end
 
-end
+    def shout(str)
+        return str.upcase
+    end
 
-def method_name
-    
-end
+    def repeat(str,n)
+        a = "#{str}" * n
+        return a.inject " " # pas fini je pense, pas sur que space marche
+
+    end
+
+    def start_of_word(str,n)
+        return str[0,n]
+    end
+
+    def first_word(arr)
+        arr.split(" ")[0]
+    end
+
+    def titleize(arr)
+
+
+    end
---
+++ lib/05_timer.rb
@@ -0,0 +1,9 @@
+
+def time_string(sec)
+    # p Time.new(4000)
+    # p "%02d:%02d:%02d" % [4, 6, 7]
+    # sec = Time.strftime("%T")
+    # %H%z%M%z%S => %T
+end
+
+ time_string(4000)
